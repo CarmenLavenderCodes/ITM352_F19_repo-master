34c84a9177d2cd612bdd051bcb6a456968e2674e
CarmenLavenderCodes <54662244+CarmenLavenderCodes@users.noreply.github.com>
2019-11-22 15:23:57 -1000

    Update makingChange.js

--- Repos/makingChange.js
+++ Repos/makingChange.js
@@ -1,19 +1,4 @@
-/*
-var change = 175;//the change amounts to $1.75
-var pennies = 1;//penny worth 1 cent
-var nickles = 5;// nickle worth 5 cents
-var dimes = 10;// dime worth 10 cents
-var quarter = 25;// quarter worth 25 cents
-step1 = change % quarter;//how many quarters go into $1.75
-change = change - step1;//subtracting the number of quarters from the change
-step2 = change % dimes;//dimes that are needed to fit the new change
-change = change - step2;// subtracting the number of dimes from the change
-step3 = change % nickles;//nickles that are needed to fit the new change
-change = change - step3;// subtracting the number of nickles from the change
-change = change;//show the rest of the pennies needed
-console.log("number of quarters" + step1 + "number of dimes" + step2 + "number of nickles" + step3 +
- "number of pennies" + change);
-*/
+
 
  // Coin values = [25, 10, 5, 1]
 
